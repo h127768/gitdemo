d9ed0dc01a4b85ecffd5988392da95e20f695f53
tonynguyen <tonynguyen@192.168.1.138>
2020-09-02 00:13:41 +0200

    Renew

--- Innlevering_old/src/no/hvl/dat100/Oppgave2.java
+++ Innlevering_old/src/no/hvl/dat100/Oppgave2.java
@@ -2,8 +2,6 @@
  * Lag et program for å finne alle oddetallene 
  * fra og med en nedre grense til og med en øvre grense.
  * 
- * Hint Avgjør først om den nedre grensen er et oddetall (bruk operatoren %).
- * 
  */
 package no.hvl.dat100;
 
@@ -14,33 +12,31 @@ public class Oppgave2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Finn alle oddetallene");
+		int x1 = parseInt(showInputDialog("Oppgi tall 1: "));
+		int x2 = parseInt(showInputDialog("Oppgi tall 2: "));
 		
 		int nedreGrense = 0;
 		int ovreGrense = 0;
-		int resultat = 0;
 		
-		for(int i = 1; i <= 5; i++) {
-			int tall = parseInt(showInputDialog("Oppgi tall nr " +i));
-		
-			if(i == 1) {
-				nedreGrense = tall;
-			}
-			
-			if(i == 5) {
-				ovreGrense = tall;
-			}
-			
-			if(tall % 2 != 0) {
-				resultat = tall;
-				System.out.print(false);
-				
-			}
+		if(x1 > x2) {
+			nedreGrense = x2;
+			ovreGrense = x1;
+		}else {
+			nedreGrense = x1;
+			ovreGrense = x2;
 		}
 		
-		showMessageDialog(null, "Nedre grense " + nedreGrense +"\n" 
-		+ "Øvre grense "+ ovreGrense);
+		System.out.println("Alle oddetallene mellom " + nedreGrense +" og " +ovreGrense);
+		System.out.println();
+		
+		for(int tall = nedreGrense; tall <= ovreGrense; tall++){
+			if(tall % 2 != 0) {
+				System.out.print(tall + " ");
+			}
+			
+		}
+		
+		showMessageDialog(null,"Nedre grense " +nedreGrense +"\n" + "Øvre grense " +ovreGrense);
 		
 	}
-
 }
--- Innlevering_old/src/no/hvl/dat100/Oppgave5c.java
+++ Innlevering_old/src/no/hvl/dat100/Oppgave5c.java
@@ -1,3 +1,9 @@
+/*
+ * Legg inn kontroll på innlesinga i programmet i pkt. b) 
+ * slik at ugyldige poengsummer (negativ verdi eller over 100) må leses inn på nytt.
+ * 
+ */
+
 package no.hvl.dat100;
 
 import static java.lang.Integer.*;
@@ -6,30 +12,30 @@ import static javax.swing.JOptionPane.*;
 public class Oppgave5c {
 
 	public static void main(String[] args) {
-		int k = 1;
-		
+	
 		for (int i=1; i<=10; i++) {
-			int poeng = parseInt(showInputDialog("Oppgitt poengsum for student " +i));
+			int poeng = parseInt(showInputDialog("Oppgi poengsum for student " +i));
 			if (poeng <0 || poeng>100){
 				showMessageDialog(null, "Ugyldig poengsum !");
+				i = i-1;
 			} else {
 				if (poeng>=0 && poeng<=39){
-					showMessageDialog(null, "Student " + i + ": Karakter F");
+					showMessageDialog(null, "Student " + i +": Karakter F");
 				}
 				else if (poeng>=40 && poeng<=49){
-					showMessageDialog(null, "Student " + i + ": Karakter E");
+					showMessageDialog(null, "Student " + i +": Karakter E");
 				}
 				else if (poeng>=50 && poeng<=59){
-					showMessageDialog(null, "Student " + i + ": Karakter D");
+					showMessageDialog(null, "Student " + i +": Karakter D");
 				}
 				else if (poeng>=60 && poeng<=79){
-					showMessageDialog(null, "Student " + i + ": Karakter C");
+					showMessageDialog(null, "Student " + i +": Karakter C");
 				}
 				else if (poeng>=80 && poeng<=89){
-					showMessageDialog(null, "Student " + i + ": Karakter B");
+					showMessageDialog(null, "Student " + i +": Karakter B");
 				}
 				else {
-					showMessageDialog(null, "Student " + i + ": Karakter A");
+					showMessageDialog(null, "Student " + i +": Karakter A");
 				}
 			}
 		}
